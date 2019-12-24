5f35ea97da7b366bc680a1595ca5f1ecd2ea9572
lxt3063 <924638649@qq.com>
2019-12-24 22:47:05 +0800

    first commit

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"'
+  BASE_API: '"http://129.204.103.63:8080"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://120.27.63.9:8080"'
+  BASE_API: '"http://129.204.103.63:8080"'
 }
